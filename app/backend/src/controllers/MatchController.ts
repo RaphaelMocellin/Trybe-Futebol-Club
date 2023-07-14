1baa433cd8257d5bc84dc8f1ca9d89599804bd76
rapha <raphael.mocellin@gmail.com>
2023-07-14 13:18:04 -0300

    Elaborando o req 15

---
+++ app/backend/src/controllers/MatchController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import MatchService from '../services/MatchService';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
+
+export default class MatchController {
+  constructor(
+    private matchService = new MatchService(),
+  ) { }
+
+  public async getAllMatches(_req: Request, res: Response) {
+    const serviceResponse = await this.matchService.getAllMatches();
+    res.status(200).json(serviceResponse.data);
+  }
+
+  public async getMatchesById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const serviceResponse = await this.matchService.getMatchById(Number(id));
+
+    if (serviceResponse.status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    res.status(200).json(serviceResponse.data);
+  }
+
+  public async createMatch(req: Request, res: Response) {
+    const serviceResponse = await this.matchService.createMatch(req.body);
+    res.status(201).json(serviceResponse.data);
+  }
+
+  public async updateMatch(req: Request, res: Response): Promise<Response> {
+    const id = Number(req.params.id);
+    const match = req.body;
+    const serviceResponse = await this.matchService.updateMatch(id, match);
+
+    if (serviceResponse.status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    return res.status(200).json(serviceResponse.data);
+  }
+
+  public async deleteTeam(req: Request, res: Response): Promise<Response> {
+    const id = Number(req.params.id);
+    const serviceResponse = await this.matchService.deleteTeam(id);
+
+    if (serviceResponse.status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    return res.status(200).json(serviceResponse.data);
+  }
+}
---
+++ app/backend/src/services/MatchService.ts
@@ -0,0 +1,47 @@
+import MatchModel from '../models/MatchModel';
+import { IMatchModel } from '../Interfaces/matches/IMatchModel';
+import { NewEntity } from '../Interfaces';
+import { IMatch } from '../Interfaces/matches/IMatch';
+import { ServiceMessage, ServiceResponse } from '../Interfaces/ServiceResponse';
+
+export default class MatchService {
+  constructor(
+    private matchModel: IMatchModel = new MatchModel(),
+  ) { }
+
+  public async getAllMatches(): Promise<ServiceResponse<IMatch[]>> {
+    const allMatches = await this.matchModel.findAll();
+    return { status: 'SUCCESSFUL', data: allMatches };
+  }
+
+  public async getMatchById(id: number): Promise<ServiceResponse<IMatch>> {
+    const match = await this.matchModel.findById(id);
+    if (!match) return { status: 'NOT_FOUND', data: { message: `Match ${id} not found` } };
+    return { status: 'SUCCESSFUL', data: match };
+  }
+
+  public async createMatch(match: NewEntity<IMatch>): Promise<ServiceResponse<IMatch>> {
+    const newMatch = await this.matchModel.create(match);
+    return { status: 'SUCCESSFUL', data: newMatch };
+  }
+
+  public async updateMatch(id: number, match: IMatch): Promise<ServiceResponse<ServiceMessage>> {
+    const matchFound = await this.matchModel.findById(id);
+    if (!matchFound) return { status: 'NOT_FOUND', data: { message: `Match ${id} not found` } };
+
+    const updatedMatch = await this.matchModel.update(id, match);
+    if (!updatedMatch) {
+      return { status: 'CONFLICT',
+        data: { message: `There are no updates to perform in Match ${id}` } };
+    }
+    return { status: 'SUCCESSFUL', data: { message: 'Match updated' } };
+  }
+
+  public async deleteMatch(id: number): Promise<ServiceResponse<ServiceMessage>> {
+    const matchFound = await this.matchModel.findById(id);
+    if (!matchFound) return { status: 'NOT_FOUND', data: { message: `Match ${id} not found` } };
+
+    await this.matchModel.delete(id);
+    return { status: 'SUCCESSFUL', data: { message: 'Match deleted' } };
+  }
+}
