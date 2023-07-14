f097fbf8638e7dda7d010acddf769dd07ef9f3d6
rapha <raphael.mocellin@gmail.com>
2023-07-13 21:51:08 -0300

    Elaborado o Authcontroller para lidar com a rota de login e o JwtUtils para lidar com o token

---
+++ app/backend/src/controllers/AuthController.ts
@@ -0,0 +1,34 @@
+import * as bcrypt from 'bcryptjs';
+import { Request, Response } from 'express';
+import JwtUtils from '../utils/JwtUtils';
+import { ILogin } from '../Interfaces/users/IUser';
+import UserModel from '../models/UserModel';
+
+export default class AuthController {
+  private jwtUtils = new JwtUtils();
+
+  private model: UserModel = new UserModel();
+
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body as ILogin;
+    const user = await this.model.findByEmail(email);
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'user not found',
+      });
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({
+        message: 'Invalid email or password',
+      });
+    }
+
+    const token = this.jwtUtils.sign({ id: user.id });
+
+    return res.status(200).json({
+      token,
+    });
+  }
+}
