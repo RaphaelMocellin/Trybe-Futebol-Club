abf4b0b2786cd7692405fbd83665cc3f0c74968c
rapha <raphael.mocellin@gmail.com>
2023-07-17 14:45:00 -0300

    Elaborada a classe leaderboard... eu acho!

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -11,15 +11,20 @@ import { ITeams } from '../Interfaces/teams/ITeams';
 import { IMatch } from '../Interfaces/matches/IMatch';
 
 import { ServiceMessage, ServiceResponse } from '../Interfaces/ServiceResponse';
+import { ITeamInfo } from '../Interfaces/teams/ITeamInfo';
+import TeamInfo from '../utils/TeamInfo';
 
 export default class leaderboardService {
   constructor(
     private teamModel: ITeamsModel = new TeamModel(),
     private matchModel: MatchModel = new MatchModel(),
+    private teamInfo: ITeamInfo = new TeamInfo()
   ) {}
 
   public async getGeneralLeaderboard(): Promise<ServiceResponse<ILeaderboard[]>> {
     const allTeams = await this.teamModel.findAll();
     const allMatches = await this.matchModel.findByQuery('inProgress', false);
+
+    const leaderboard = allTeams.map((team) => new )
   }
 }
--- app/backend/src/utils/TeamInfo.ts
+++ app/backend/src/utils/TeamInfo.ts
@@ -4,6 +4,8 @@ import { ITeamInfo } from '../Interfaces/teams/ITeamInfo';
 export default class TeamInfo implements ITeamInfo {
   public name;
 
+  public teamId;
+
   public totalPoints = 0;
 
   public totalGames = 0;
@@ -22,8 +24,9 @@ export default class TeamInfo implements ITeamInfo {
 
   public efficiency = 0;
 
-  constructor(clubName: string, matches: IMatch[]) {
-    this.name = clubName;
+  constructor(teamName: string, teamId: number, matches: IMatch[]) {
+    this.name = teamName;
+    this.teamId = teamId;
     this.totalGames = matches.length;
     this.calcAllStats(matches);
     this.goalsBalance = this.goalsFavor - this.goalsOwn;
@@ -32,38 +35,57 @@ export default class TeamInfo implements ITeamInfo {
 
   private calcTotalPoints(matches: IMatch[]) {
     return matches.forEach((match) => {
-      if (match.homeTeamGoals > match.awayTeamGoals) this.totalPoints += 3;
-      if (match.homeTeamGoals === match.awayTeamGoals) this.totalPoints += 1;
+      if (match.homeTeamId === this.teamId) {
+        if (match.homeTeamGoals > match.awayTeamGoals) this.totalPoints += 3;
+        if (match.homeTeamGoals === match.awayTeamGoals) this.totalPoints += 1;
+      }
+      if (match.awayTeamId === this.teamId) {
+        if (match.awayTeamGoals > match.homeTeamGoals) this.totalPoints += 3;
+        if (match.awayTeamGoals === match.homeTeamGoals) this.totalPoints += 1;
+      }
     });
   }
 
   private calcTotalVictories(matches: IMatch[]) {
     return matches.forEach((match) => {
-      if (match.homeTeamGoals > match.awayTeamGoals) this.totalVictories += 1;
+      if (match.homeTeamId === this.teamId
+        && match.homeTeamGoals > match.awayTeamGoals) this.totalVictories += 1;
+
+      if (match.awayTeamId === this.teamId
+        && match.awayTeamGoals > match.homeTeamGoals) this.totalVictories += 1;
     });
   }
 
   private calcTotalDraws(matches: IMatch[]) {
     return matches.forEach((match) => {
-      if (match.homeTeamGoals === match.awayTeamGoals) this.totalDraws += 1;
+      if (match.homeTeamId === this.teamId
+        && match.homeTeamGoals === match.awayTeamGoals) this.totalDraws += 1;
+
+      if (match.awayTeamId === this.teamId
+        && match.awayTeamGoals === match.homeTeamGoals) this.totalDraws += 1;
     });
   }
 
   private calcTotalLosses(matches: IMatch[]) {
     return matches.forEach((match) => {
-      if (match.homeTeamGoals < match.awayTeamGoals) this.totalLosses += 1;
+      if (match.homeTeamId === this.teamId
+        && match.homeTeamGoals < match.awayTeamGoals) this.totalLosses += 1;
+      if (match.awayTeamId === this.teamId
+        && match.awayTeamGoals < match.homeTeamGoals) this.totalLosses += 1;
     });
   }
 
   private calcGoalsFavor(matches: IMatch[]) {
     matches.forEach((match) => {
-      this.goalsFavor += match.homeTeamGoals;
+      if (match.homeTeamId === this.teamId) this.goalsFavor += match.homeTeamGoals;
+      if (match.awayTeamId === this.teamId) this.goalsFavor += match.awayTeamGoals;
     });
   }
 
   private calcGoalsOwn(matches: IMatch[]) {
     matches.forEach((match) => {
-      this.goalsOwn += match.awayTeamGoals;
+      if (match.homeTeamId === this.teamId) this.goalsOwn += match.awayTeamGoals;
+      if (match.awayTeamId === this.teamId) this.goalsOwn += match.homeTeamGoals;
     });
   }
 
